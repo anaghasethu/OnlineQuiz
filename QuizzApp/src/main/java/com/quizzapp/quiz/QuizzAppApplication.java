f0eb8ddf3cad233e2a20083b4141d38dba5c8aee
unknown <udayasrichandrika5@gmail.com>
2022-08-06 07:05:43 +0530

    Category class added

---
+++ QuizzApp/src/main/java/com/quizzapp/quiz/QuizzAppApplication.java
@@ -0,0 +1,13 @@
+package com.quizzapp.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzAppApplication.class, args);
+	}
+
+}
